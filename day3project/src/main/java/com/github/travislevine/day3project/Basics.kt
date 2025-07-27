496aa084b41694eecee8bcd14b0b03c22770d514
Travis <sneakyplutosaturn@gmail.com>
2025-07-27 21:02:25 +1000

    Day 3 Project - basic input

--- day3project/src/main/java/com/github/travislevine/day3project/Basics.kt
+++ day3project/src/main/java/com/github/travislevine/day3project/Basics.kt
@@ -1,19 +1,23 @@
 package com.github.travislevine.day3project
 
 fun main() {
-    makeCoffee(1, "Travis")
-    makeCoffee(3, "Ting")
-    makeCoffee(0, "Leo")
+
 
 }
 
+fun askCoffeeDetails () {
+    println("Who is this coffee for? ")
+    var name = readln()
+    println("How many spoons of sugar do you want? ")
+    var sugarCount = readln().toInt()
+    makeCoffee(sugarCount, name)
 
-fun makeCoffee(sugarCount : Int, name : String){
-    if (sugarCount == 1)
-    println("Coffee with $sugarCount spoon of sugar for $name")
-    else if (sugarCount > 1)
-        println("Coffee with $sugarCount spoons of sugar for $name")
-    else if (sugarCount == 0) {
+}
+fun makeCoffee(sugarCount: Int, name: String) {
+    if (sugarCount == 1) {
+        println("Coffee with $sugarCount spoon of sugar for $name")
+    } else if (sugarCount == 0) {
         println("Coffee with no sugar for $name")
-    }
+    } else if (sugarCount > 1)
+        println("Coffee with $sugarCount spoons of sugar for $name")
 }
