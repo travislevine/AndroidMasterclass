1a281441061e4072269252703c5510fc679b8927
Travis <sneakyplutosaturn@gmail.com>
2025-07-31 08:54:56 +1000

    feat: Day 3 Project - userinputDouble() with double returns. Understand integer division.

--- day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
+++ day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
@@ -2,24 +2,39 @@ package com.github.travislevine.day3project
 
 fun main() {
     println("Enter a number to add: ")
-    val num1 = userInput()
+    val num1 = userInputInt()
     println("Enter another number to add: ")
-    val num2 = userInput()
+    val num2 = userInputInt()
     val myInt = add(num2,  num1)
     println("The result of num1 + num2 is: $myInt")
+
+    println("Enter a number to divide: ")
+    val n1 = userInputDouble()
+    println("Enter another number to add: ")
+    val n2 = userInputDouble()
+    val myDouble = divide(n1, n2)
+    println("The result of num1 \\ num2 is: $myDouble")
 }
 
-fun userInput() : Int  {
+fun userInputInt() : Int  {
     val num = readln().toInt()
     return num
 }
+
+fun userInputDouble() : Double  {
+    return readln().toDouble()
+}
+
 
 fun  add(num1:Int, num2:Int): Int {
     val result =  num1 + num2
     return result
 }
 
-
+fun divide(num1: Double, num2: Double) : Double {
+    val result = num1 / num2.toDouble()
+    return result
+}
 fun askCoffeeDetails () {
     println("Who is this coffee for? ")
     val name = readln()
