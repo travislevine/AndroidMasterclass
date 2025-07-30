304fff5f88226fa3a4aaa34b690eca46a613f5ce
Travis <sneakyplutosaturn@gmail.com>
2025-07-31 08:19:48 +1000

    feat: Day 3 Project - userinput() with integer returns

--- day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
+++ day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
@@ -1,15 +1,30 @@
 package com.github.travislevine.day3project
 
 fun main() {
-
-
+    println("Enter a number to add: ")
+    val num1 = userInput()
+    println("Enter another number to add: ")
+    val num2 = userInput()
+    val myInt = add(num2,  num1)
+    println("The result of num1 + num2 is: $myInt")
 }
+
+fun userInput() : Int  {
+    val num = readln().toInt()
+    return num
+}
+
+fun  add(num1:Int, num2:Int): Int {
+    val result =  num1 + num2
+    return result
+}
+
 
 fun askCoffeeDetails () {
     println("Who is this coffee for? ")
-    var name = readln()
+    val name = readln()
     println("How many spoons of sugar do you want? ")
-    var sugarCount = readln().toInt()
+    val sugarCount = readln().toInt()
     makeCoffee(sugarCount, name)
 
 }
