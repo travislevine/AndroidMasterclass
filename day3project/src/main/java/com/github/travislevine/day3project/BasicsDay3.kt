60b69459a53bd4fb27c9c0f1a52459c263502684
Travis <sneakyplutosaturn@gmail.com>
2025-07-31 08:57:12 +1000

    feat: Day 3 Project - Add ReturnTypesDay3: refactoring file structure.

--- day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
+++ day3project/src/main/java/com/github/travislevine/day3project/BasicsDay3.kt
@@ -1,19 +1,7 @@
 package com.github.travislevine.day3project
 
 fun main() {
-    println("Enter a number to add: ")
-    val num1 = userInputInt()
-    println("Enter another number to add: ")
-    val num2 = userInputInt()
-    val myInt = add(num2,  num1)
-    println("The result of num1 + num2 is: $myInt")
 
-    println("Enter a number to divide: ")
-    val n1 = userInputDouble()
-    println("Enter another number to add: ")
-    val n2 = userInputDouble()
-    val myDouble = divide(n1, n2)
-    println("The result of num1 \\ num2 is: $myDouble")
 }
 
 fun userInputInt() : Int  {
@@ -26,15 +14,7 @@ fun userInputDouble() : Double  {
 }
 
 
-fun  add(num1:Int, num2:Int): Int {
-    val result =  num1 + num2
-    return result
-}
 
-fun divide(num1: Double, num2: Double) : Double {
-    val result = num1 / num2.toDouble()
-    return result
-}
 fun askCoffeeDetails () {
     println("Who is this coffee for? ")
     val name = readln()
