6ba62c8cc98093ce88e7db5399216c32d16c61d1
Travis <sneakyplutosaturn@gmail.com>
2025-08-04 11:34:06 +1000

    feat: Day 23 (XML) Project, Complete date picker with clickDatePicker method.

--- day23dobcalc/src/main/java/com/github/travislevine/day23dobcalc/MainActivity.kt
+++ day23dobcalc/src/main/java/com/github/travislevine/day23dobcalc/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.github.travislevine.day23dobcalc
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+        // Gives us access to day of the year, month, year etc
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                Toast.makeText(
+                    this,
+                    "DatePickerDialog", Toast.LENGTH_LONG).show()
+            },
+            year,
+            month,
+            day
+            ).show()
+
+
     }
 }
